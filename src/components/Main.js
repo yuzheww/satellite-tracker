1f304ae64fcc81d35c6448b7f71c5fc92b3caf0a
yuzheww <yuzhe.wang@vanderbilt.edu>
2020-08-19 12:24:22 +0800

    add body

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="main">
+        <div className="left-side">left</div>
+        <div className="right-side">right</div>
+      </div>
+    );
+  }
+}
+export default Main;
